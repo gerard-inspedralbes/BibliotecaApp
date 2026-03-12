0030c16d8bb85b42257d5003a77a8c7e27bd4734
gtorrents <gtorrents@inspedralbes.cat>
2026-03-12 08:45:15 +0100

    Modificada classe Reserva per afegir atributs Llibre, Usuari, un constructor, getters i toStirng

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Reserva {
+    private Usuari usuari;
+    private Llibre llibre;
+
+    public Reserva(Usuari usuari, Llibre llibre) {
+        this.usuari = usuari;
+        this.llibre = llibre;
+    }
+
+    public Usuari getUsuari() {
+        return usuari;
+    }
+
+    public Llibre getLlibre() {
+        return llibre;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "usuari=" + usuari +
+                ", llibre=" + llibre +
+                '}';
+    }
 }
